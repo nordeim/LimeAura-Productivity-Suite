422ccf672294cfa81e2b06ec119a662cd7758f0a
nordeim <nordeimkuahsziyp@outlook.com>
2025-11-18 05:36:25 +0800

    Create index.ts

---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @file packages/api-client/src/index.ts
+ * @purpose Main entry point for the API client package.
+ * @interface Public API
+ * @phase 3
+ */
+
+export * from './client';
+export * from './interceptors';
