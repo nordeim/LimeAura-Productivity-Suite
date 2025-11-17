c3ca78960ed4952585302792c219c602e48d82ac
nordeim <nordeimkuahsziyp@outlook.com>
2025-11-18 06:36:58 +0800

    Create index.ts

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @file packages/ui/src/index.ts
+ * @purpose Main entry point for the @limeaura/ui package. Exports all components.
+ * @phase 4
+ */
+
+export * from './components/Button/Button';
+export * from './components/Input/Input';
+export * from './components/Avatar/Avatar';
+export * from './components/Badge/Badge';
+export * from './components/Icon/Icon';
+export * from './components/Spinner/Spinner';
+export * from './components/Toggle/Toggle';
+export * from './components/Tooltip/Tooltip';
