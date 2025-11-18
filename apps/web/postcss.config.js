34943a5da4f54b350bd94f8a67295d3817401944
nordeim <nordeimkuahsziyp@outlook.com>
2025-11-18 13:12:41 +0800

    Create postcss.config.js

---
+++ apps/web/postcss.config.js
@@ -0,0 +1,11 @@
+/**
+ * @file apps/web/postcss.config.js
+ * @purpose PostCSS configuration.
+ * @phase 7
+ */
+export default {
+  plugins: {
+    // We will add plugins like autoprefixer later if needed.
+    // CSS Modules are handled by Vite's config.
+  },
+};
